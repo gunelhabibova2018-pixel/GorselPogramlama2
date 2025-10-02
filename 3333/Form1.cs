1a074790ec82e528c9c76a25195553a7835f8f15
gunelhabibova2018-pixel <gunelhabibova2018@gmail.com>
2025-10-02 11:49:43 +0300

    Add files via upload

---
+++ 3333/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3333
+{
+    public partial class Form1 : Form
+    {private bool isDragging=false;
+        private Point lastLocation;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDragging = true;
+            lastLocation=e.Location;
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging) {
+                button1.Location=new Point((e.X - lastLocation.X)+button1.Left, (e.Y - lastLocation.Y)+button1.Top);
+                }
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging=false;
+        }
+    }
+}
