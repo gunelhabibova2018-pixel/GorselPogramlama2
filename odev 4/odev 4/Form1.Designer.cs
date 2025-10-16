a1d5ba6ea3dd8b360720dcc96b5291c167baa0e4
gunelhabibova2018-pixel <gunelhabibova2018@gmail.com>
2025-10-16 10:24:15 +0300

    odev4

---
+++ odev 4/odev 4/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace odev_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Location = new System.Drawing.Point(550, 58);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(13, 13);
+            this.labelScore.TabIndex = 0;
+            this.labelScore.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(550, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PUAN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 505);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelScore);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ odev 4/odev 4/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odev_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+            timer1.Interval = 1000; 
+            timer1.Start();
+        }
+        Random rnd = new Random();
+        int score = 0;
+
+        
+        Rectangle targetArea = new Rectangle(200, 150, 300, 300);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            CreateRandomButton();
+        }
+        private void CreateRandomButton()
+        {
+            int number = rnd.Next(1, 11); 
+            Button btn = new Button();
+            btn.Width = 50;
+            btn.Height = 50;
+
+            int x = rnd.Next(this.ClientSize.Width - btn.Width);
+            int y = rnd.Next(this.ClientSize.Height - btn.Height);
+            btn.Location = new Point(x, y);
+
+            btn.Text = number.ToString();
+
+            
+            bool inTargetArea = targetArea.Contains(btn.Bounds);
+
+            if (inTargetArea)
+                btn.ForeColor = Color.Red;
+            else
+                btn.ForeColor = Color.Black;
+
+            
+            btn.Click += (s, e) =>
+            {
+                if (btn.ForeColor == Color.Red)
+                    score += number;
+                else
+                    score -= number;
+
+                UpdateScore();
+                btn.Dispose(); 
+            };
+
+            this.Controls.Add(btn);
+            btn.BringToFront();
+        }
+
+        private void UpdateScore()
+        {
+            labelScore.Text = score.ToString();
+
+            if (score >= 100)
+            {
+                timer1.Stop();
+                MessageBox.Show("Kazandınız!", "Tebrikler", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        
+    }
+}
