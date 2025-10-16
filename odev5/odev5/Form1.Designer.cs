6d1b53c16badb914b804b2826ac8c70d8a170e39
gunelhabibova2018-pixel <gunelhabibova2018@gmail.com>
2025-10-16 12:17:11 +0300

    odev5

---
+++ odev5/odev5/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace odev5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBitir = new System.Windows.Forms.Button();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.countdownTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(644, 289);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(647, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Süre";
+            // 
+            // btnBitir
+            // 
+            this.btnBitir.Location = new System.Drawing.Point(647, 180);
+            this.btnBitir.Name = "btnBitir";
+            this.btnBitir.Size = new System.Drawing.Size(75, 23);
+            this.btnBitir.TabIndex = 2;
+            this.btnBitir.Text = "Bitir";
+            this.btnBitir.UseVisualStyleBackColor = true;
+            this.btnBitir.Click += new System.EventHandler(this.btnBitir_Click);
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Location = new System.Drawing.Point(31, 180);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(75, 23);
+            this.btnBasla.TabIndex = 2;
+            this.btnBasla.Text = "Oyuna Başla";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(138, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(327, 426);
+            this.panel1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(471, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 420);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnBasla);
+            this.Controls.Add(this.btnBitir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBitir;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer countdownTimer;
+    }
+}
+
---
+++ odev5/odev5/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odev5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        List<Button> numberButtons = new List<Button>();
+        Timer moveTimer = new Timer();
+        
+        int remainingTime = 60; // 60 saniye
+        List<int> clickedEvenNumbers = new List<int>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void StartGame()
+        {
+            // 10 adet sayı butonunu oluştur
+            for (int i = 0; i < 10; i++)
+            {
+                Button btn = new Button();
+                btn.Size = new Size(50, 50);
+                btn.Text = rnd.Next(1, 101).ToString();
+                btn.Click += NumberButton_Click;
+                btn.BackColor = Color.White;
+                numberButtons.Add(btn);
+                panel1.Controls.Add(btn);
+            }
+
+            // İlk rastgele konumlandırma
+            MoveButtons();
+
+            // Buton hareket timer
+            moveTimer.Interval = 500; // 0.5 saniye
+            moveTimer.Tick += (s, e) => MoveButtons();
+            moveTimer.Start();
+
+            // Geri sayım timer
+            countdownTimer.Interval = 1000;
+            countdownTimer.Tick += CountdownTimer_Tick;
+            countdownTimer.Start();
+
+            label1.Text = $"Süre: {remainingTime} sn";
+        }
+
+        private void NumberButton_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            int number = int.Parse(btn.Text);
+
+            if (number % 2 == 0) // çiftse
+            {
+                clickedEvenNumbers.Add(number);
+            }
+
+            btn.Visible = false; // kaybolsun
+            listBox1.Items.Add(number);
+        }
+
+        private void CountdownTimer_Tick(object sender, EventArgs e)
+        {
+            remainingTime--;
+            label1.Text = $"Süre: {remainingTime} sn";
+
+            if (remainingTime <= 0)
+            {
+                EndGame(timeOut: true);
+            }
+        }
+
+        private void MoveButtons()
+        {
+            foreach (var btn in numberButtons)
+            {
+                if (!btn.Visible) continue; // görünmeyenleri taşımıyoruz
+                int maxX = panel1.Width - btn.Width;
+                int maxY = panel1.Height - btn.Height;
+
+                btn.Location = new Point(rnd.Next(0, maxX), rnd.Next(0, maxY));
+            }
+        }
+
+        private void btnBitir_Click(object sender, EventArgs e)
+        {
+            EndGame(timeOut: false);
+        }
+
+        private void EndGame(bool timeOut)
+        {
+            moveTimer.Stop();
+            countdownTimer.Stop();
+
+            if (timeOut)
+            {
+                MessageBox.Show("Süre doldu! Doğru tamamlayamadınız");
+                return;
+            }
+
+            // ListBox'taki çift sayıları sırala (Bubble Sort)
+            List<int> sortedClicked = new List<int>(clickedEvenNumbers);
+            BubbleSort(sortedClicked);
+
+            // Küçük çift sayılar (1-100 arası) içinden 1. çiftleri belirleyelim
+            List<int> allEvenNumbers = new List<int>();
+            for (int i = 2; i <= 100; i += 2)
+                allEvenNumbers.Add(i);
+
+            // Tıklanması gereken en küçük çift sayılar
+            List<int> smallestEvens = new List<int>();
+            for (int i = 0; i < clickedEvenNumbers.Count; i++)
+            {
+                smallestEvens.Add(allEvenNumbers[i]);
+            }
+
+            // Karşılaştırma
+            bool correct = true;
+            if (sortedClicked.Count != smallestEvens.Count)
+            {
+                correct = false;
+            }
+            else
+            {
+                for (int i = 0; i < sortedClicked.Count; i++)
+                {
+                    if (sortedClicked[i] != smallestEvens[i])
+                    {
+                        correct = false;
+                        break;
+                    }
+                }
+            }
+
+            if (correct)
+                MessageBox.Show($"Doğru tamamladınız! Kalan süre: {remainingTime} saniye");
+            else
+                MessageBox.Show($"Yanlış tamamladınız! Kalan süre: {remainingTime} saniye");
+        }
+
+        private void BubbleSort(List<int> list)
+        {
+            int n = list.Count;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (list[j] > list[j + 1])
+                    {
+                        int temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+    }
+}
